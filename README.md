d901ce01f641659cd750b3fe98b8433d20bd112d
Ali Osman <106361546+osmandlsmn@users.noreply.github.com>
2022-12-20 22:53:35 +0300

    🚀 init project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Scss Gulp Boilerplate
+
+Compiler project example with gulp for Sass
+
+## Available Scripts
+
+In the project directory, you can run:
+
+Runs the app in the development mode.
+
+### `npm start`
+
+Uygulamayı üretim modunda çalıştırır.
+
+### `npm run build`
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+"use strict";
+var gulp = require("gulp"),
+  browserSync = require("browser-sync").create(),
+  sass = require("gulp-sass")(require("sass"));
+
+gulp.task("sass", () =>
+  gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+        noCache: true,
+      })
+    )
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream())
+);
+
+gulp.task("watch", () => {
+  browserSync.init({
+    server: "./",
+    startPath: "/src/index.html",
+  });
+  gulp.watch("src/scss/**/*.scss", gulp.series("sass"));
+  gulp.watch("src/*.html").on("change", browserSync.reload);
+});
